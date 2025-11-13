31a20b03510f5badeb8c39921d984692d05a87a5
5XCode <lava4ggaming@gmail.com>
2025-11-13 22:42:43 +0200

    Update vector2d.js

--- js/vector2d.js
+++ js/vector2d.js
@@ -1,1 +1,62 @@
 
+class Vector2D {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v) {
+        return new Vector2D(this.x + v.x, this.y + v.y);
+    }
+
+    subtract(v) {
+        return new Vector2D(this.x - v.x, this.y - v.y);
+    }
+
+    multiply(scalar) {
+        return new Vector2D(this.x * scalar, this.y * scalar);
+    }
+
+    divide(scalar) {
+        return new Vector2D(this.x / scalar, this.y / scalar);
+    }
+
+    magnitude() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    normalize() {
+        const mag = this.magnitude();
+        return mag > 0 ? this.divide(mag) : new Vector2D();
+    }
+
+    distanceTo(v) {
+        return this.subtract(v).magnitude();
+    }
+
+    dot(v) {
+        return this.x * v.x + this.y * v.y;
+    }
+
+    clone() {
+        return new Vector2D(this.x, this.y);
+    }
+
+    toString() {
+        return `(${this.x}, ${this.y})`;
+    }
+
+    static fromAngle(angle, magnitude = 1) {
+        return new Vector2D(
+            Math.cos(angle) * magnitude,
+            Math.sin(angle) * magnitude
+        );
+    }
+
+    static random(minX, maxX, minY, maxY) {
+        return new Vector2D(
+            minX + Math.random() * (maxX - minX),
+            minY + Math.random() * (maxY - minY)
+        );
+    }
+}
